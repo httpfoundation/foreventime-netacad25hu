25965246ab43befcb6246fa0aa47857dbc6a0eac
Zoltan <zoltan.sisak@gmail.com>
2022-09-29 20:37:46 +0200

    Code Cleaning

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,135 +1,18 @@
-//import { useStages } from "../../Store"
 import { PageContainer, PageTitle } from "../../components"
-import { DashboardElement, DashboardItemType } from "../../types"
-
-
-import recepcio from "../../assets/images/recepcio.png"
-import nagyEloado from "../../assets/images/nagyeloado.png"
-import nagyEloadonyitva from "../../assets/images/nagyeloadonyitva.png"
-import szekcio1 from "../../assets/images/szekcio1.png"
-import szekcio1nyitva from "../../assets/images/szekcio1nyitva.png"
-import szekcio2 from "../../assets/images/szekcio2.png"
-import szekcio2nyitva from "../../assets/images/szekcio2nyitva.png"
-import szekcio3 from "../../assets/images/szekcio3.png"
-import szekcio4 from "../../assets/images/szekcio4.png"
-import iokCafe from "../../assets/images/iokcafe.png"
-import messageBoard from "../../assets/images/messageboard.png"
-import galeria from "../../assets/images/galeria.png"
-
+import { DashboardElement } from "../../types"
 import Dashboard from "../../components/Dashboard"
-import Welcome from "../Welcome";
 import { useMemo } from "react"
-import { useLiveStaticElements, useDashboardElements } from "../../Store"
-
-
-
+import { useDashboardElements } from "../../Store"
 
 const Home = () => {
 
-    const { galleryUrl } = useLiveStaticElements()
     const  dashboardElements  = useDashboardElements("home")
-    console.log("dashboardElements", dashboardElements)
-
-
     const homeDashboardItems : DashboardElement[] = useMemo(() => 
         [...dashboardElements], 
         [dashboardElements]
     )
 
-/*     const homeDashboardItems : DashboardItemType[] = useMemo(() => [
-
-        {
-            caption: "Konferenciaterem",
-            title: "Délelőtti plenáris előadások a nagyelőadóban",
-            img: nagyEloado,
-            hoverImg: nagyEloadonyitva,
-            corner: "br",
-            light: false,
-            link: "/szekcio/plenaris",
-            mobileOrder: 1
-        },
-        {
-            caption: "1. szekcióterem",
-            title: "Szakképzési, ITMP és NetAcad szekció előadásai a 1. szekcióteremben",
-            img: szekcio1,
-            hoverImg: szekcio1nyitva,
-            corner: "none",
-            light: false,
-            link: "/szekcio/szakkepzes-itmp-netacad",
-            mobileOrder: 2
-        },
-        {
-            caption: "2. szekcióterem",
-            title: "Digitális kultúra szekció előadásai a 2. szekcióteremben",
-            img: szekcio2,
-            hoverImg: szekcio2nyitva,
-            corner: "none",
-            light: false,
-            link: "/szekcio/digitalis-kultura",
-            mobileOrder: 3
-        },
-{
-            caption: "IOK Cafe",
-            title: "Résztvevők egymás közötti élő beszélgetése négy tematikus asztalnál az IOK kávézójában",
-            img: iokCafe,
-            corner: "bl",
-            light: true,
-            link: "/iok-cafe",
-            mobileOrder: 6
-        },
-        {
-            caption: "Galéria",
-            title: "Megnézheted az eseményen készült fótókat",
-            img: galeria,
-            corner: "bl",
-            light: true,
-            link: galleryUrl,
-            external: true,
-            mobileOrder: 6
-        },
-        {
-            caption: "Információs pult",
-            title: "Köszöntő, programfüzet, előadói tabló, visszajelző kérdőiv, támogatói szórólap, értékelő űrlap, HTTP-csapat brossúra",
-            img: recepcio,
-            corner: "tr",
-            light: true,
-            link: "/infopult",
-            mobileOrder: 0
-        },
-        {
-            caption: "3. szekcióterem",
-            title: "IT felsőoktatás szekció előadásai a 3.szekcióteremben",
-            img: szekcio3,
-            hoverImg: szekcio1nyitva,
-            corner: "none",
-            light: false,
-            link: "/szekcio/it-felsooktatas",
-            mobileOrder: 4
-        },
-        {
-            caption: "4. szekcióterem",
-            title: "Digitális kultúra az alsótagozatban előadások a 4. szekcióteremben",
-            img: szekcio4,
-            hoverImg: szekcio2nyitva,
-            corner: "none",
-            light: false,
-            link: "/szekcio/digitalis-kultura-also-tagozat",
-            mobileOrder: 5
-        },
-        {
-            caption: "Üzenőfal",
-            title: "Üzenőfal fontos üzenetekkel és felhívásokkal a szervezőktől",
-            img: messageBoard,
-            corner: "tl",
-            light: true,
-            link: "/uzenofal",
-            mobileOrder: 7
-        },
-            
-    ], [galleryUrl]) */
-    
-	
-    return (
+   return (
 
             <PageContainer container>
                 <PageTitle>IOK 2022 Aula</PageTitle>
--- src/pages/Reception/Reception.tsx
+++ src/pages/Reception/Reception.tsx
@@ -1,98 +1,11 @@
 import { PageContainer } from "../../components"
 import PageTitle from "../../components/PageTitle"
-import iokCafe from "../../assets/images/iokcafe.png"
-import welcome from "../../assets/images/welcome.png"
-import feedback from "../../assets/images/feedback.png"
-import http from "../../assets/images/http.png"
-import presenters from "../../assets/images/presenters.png"
-import sponsors from "../../assets/images/sponsors.png"
-import schedule from "../../assets/images/schedule.png"
-import handout from "../../assets/images/handout.png"
-import menucard from "../../assets/images/menucard.png"
 import {  Container, } from "@mui/material"
-
 import Dashboard from "../../components/Dashboard"
-import { DashboardElement, DashboardItemType } from "../../types"
+import { DashboardElement } from "../../types"
 import { useDashboardElements } from "../../Store"
 
 
-
-/* const receptionDashboardItems : DashboardItemType[] = [
-    {
-        caption: "Köszöntő",
-        title: "Üdvözlő szép szavak az elnök úrtól",
-        img: welcome,
-        corner: "br",
-        light: true,
-        link: "/koszonto",
-        mobileOrder: 1
-    },
-    {
-        caption: "Programfüzet",
-        title: "Minden, amit érdemes tudni a szakmai programról",
-        img: schedule,
-        corner: "none",
-        light: true,
-        link: "/eloadasok",
-        mobileOrder: 1
-    },
-    {
-        caption: "Előadói tabló",
-        title: "Előadóink bemutatkozása",
-        img: presenters,
-        corner: "none",
-        light: false,
-        link: "/eloadok",
-        mobileOrder: 1
-    },
-    {
-        caption: "Támogatói tabló",
-        title: "Támogatóink bemutatkozása",
-        img: sponsors,
-        corner: "bl",
-        light: false,
-        link: "/tamogatok",
-        mobileOrder: 1
-    },
-    {
-        caption: "Értékelő űrlap",
-        title: "Előadások és a konferencia értékelése",
-        img: feedback,
-        corner: "tr",
-        light: true,
-        link: "/ertekeles",
-        mobileOrder: 1
-    },
-    {
-        caption: "HTTP szervezőcsapat",
-        title: "A HTTP-csapat állandó és tiszteletbeli tagjai, akik nélkül nem lenne most konferencia",
-        img: http,
-        corner: "none",
-        light: true,
-        link: "/http-csapat",
-        mobileOrder: 1
-    },
-    {
-        caption: "Étlap",
-        title: "Ami az asztalra kerül...",
-        img: menucard,
-        corner: "none",
-        light: false,
-        link: "/etlap",
-        mobileOrder: 1
-    },
-    {
-        caption: "IOK VKK útmutató",
-        title: "IOK Virtuális Konferenciaközpont használati útmutató",
-        img: handout,
-        corner: "tl",
-        light: false,
-        link: "/utmutato",
-        mobileOrder: 1
-    },
-        
-]
- */
 
 const Reception = () => {
     const receptionDashboardItems : DashboardElement[] = useDashboardElements("reception")
